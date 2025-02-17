b2d29b0cb65f2d693f2e39640c79ac471d49bc6e
muthu150797 <muthumani150797@gmail.com>
2025-02-17 16:45:51 +0530

    logout button

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -92,7 +92,14 @@ const Dashboard = () => {
           <button class="openbtn" onClick={toggle}>
             ☰
           </button>
-          <a
+          <div  onClick={(e) => {
+              e.preventDefault();
+              logout();
+            }}  style="float: right; padding: 20px;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-power" viewBox="0 0 16 16">
+  <path d="M7.5 1v7h1V1z"></path>
+  <path d="M3 8.812a5 5 0 0 1 2.578-4.375l-.485-.874A6 6 0 1 0 11 3.616l-.501.865A5 5 0 1 1 3 8.812"></path>
+</svg></div>
+          {/* <a
             style={{ float: 'right', padding: '20px' }}
             href="#"
             onClick={(e) => {
@@ -101,7 +108,7 @@ const Dashboard = () => {
             }}
           >
             <span class="glyphicon glyphicon-off"></span>
-          </a>
+          </a> */}
         </div>
         {(() => {
           if (isFirstLoad) {
