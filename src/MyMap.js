9ed1590f599184a7201134db84051bc11a210e7c
muthu150797 <muthumani150797@gmail.com>
2025-02-19 11:55:48 +0530

    zczx

--- src/MyMap.js
+++ src/MyMap.js
@@ -1,13 +1,6 @@
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
 
 function MyCustomComponent() {
-    const map = useMap();
-    if (!map) {
-      console.log('Map is not available');
-      return null;
-    }
-    console.log('Map is available', map);
+   
     return <div>Map loaded</div>;
 }
 
