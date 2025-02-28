c6842ed3df1dd9ff3893965201abec0857fa30bc
muthu150797 <muthumani150797@gmail.com>
2025-02-28 10:00:33 +0530

    new

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,15 +2,14 @@ importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
 
 // Initialize Firebase inside the service worker
-firebase.initializeApp({
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+firebase.initializeApp( {
+  apiKey: "AIzaSyAYga6SFlqb5wpU2F6npP4AWotpI8yLDfM",
+  authDomain: "chat-with-my-appo.firebaseapp.com",
+  projectId: "chat-with-my-appo",
+  storageBucket: "chat-with-my-appo.firebasestorage.app",
+  messagingSenderId: "769681407653",
+  appId: "1:769681407653:web:70b9c455a4b293fd25974c"
 });
-
 const messaging = firebase.messaging();
 
 // Handle background notifications
